f74fbdcc6a5a7fb882032dc4406fd06ee2c50dda
laura antonius <laurastolen@gmail.com>
2019-12-10 10:40:19 -0800

    corrected linter filename, added cors

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+app.use(cors());
 
 app.get('/', (req, res) => res.send('hello world'));
 
